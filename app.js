30af67c5aebafa4707fa4b0cebbf5263c84ba7a8
Parnab <parnab0410@gmail.com>
2020-10-10 15:23:35 +0530

    changed structure

--- app.js
+++ app.js
@@ -3,24 +3,29 @@ const mongoose = require('mongoose')
 const app = express()
 const { MongoURI } = require('./config/keys')
 const port = process.env.PORT || 5000
+const expressLayouts = require('express-ejs-layouts')
 
-// connecting to mongodb
+// mongodb specific
 mongoose.connect(MongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
 mongoose.connection.on("connected", () => {
     console.log("Connected to Mongodb Atlas!")
 })
-
 mongoose.connection.on("error", (e) => {
     console.log(e)
 })
 
-app.get('/', (req, res) => {
-    res.send('hello')
-})
+// express specific
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+// app.set('layout', 'layouts/layout')
+// app.use(expressLayouts)
+app.use(express.static('public'))
+
+// registering all the routes
+app.use(require('./routes/auth'))
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/signin', (req, res) => {
+    res.render('index', { title: "hello parnab" })
+})
+
+module.exports = router
