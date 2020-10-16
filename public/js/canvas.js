eae52e33f7652e7ab6ce5e492de591336dbee2b5
vivek5044 <vivekshukla509@gmail.com>
2020-10-16 10:27:40 +0530

    Extra features in whiteboard

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,9 +1,43 @@
-window.addEventListener("load", () =>{
-    const canvas =document.querySelector("#canvas");
+// window.addEventListener("load", () =>{
+//     const canvas =document.querySelector("#canvas");
+//     const ctx= canvas.getContext("2d");
+//     //resizing
+//     canvas.height = window.innerHeight;
+//     canvas.width = window.innerWidth;
+     
+//     let painting=false;
+
+//     function startPosition(e){
+//         painting=true;
+//         draw(e)
+//     }
+//     function finishedPosition(){
+//         painting=false;
+//         ctx.beginPath();
+//     }
+//     function draw(e){
+//         if (!painting) return;
+//         ctx.lineWidth=5;
+//         ctx.lineCap="round";
+//         ctx.strokeStyle='red';
+
+//         ctx.lineTo(e.clientX,e.clientY);
+//         ctx.stroke();
+//         ctx.beginPath();
+//         ctx.moveTo(e.clientX,e.clientY);        
+//     }
+//     canvas.addEventListener("mousedown",startPosition);
+//     canvas.addEventListener("mouseup",finishedPosition);
+//     canvas.addEventListener("mousemove",draw);
+    
+// });
+
+
+const canvas =document.querySelector("#canvas");
     const ctx= canvas.getContext("2d");
     //resizing
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight-30;
+    canvas.width = window.innerWidth -30;
      
     let painting=false;
 
@@ -29,5 +63,3 @@ window.addEventListener("load", () =>{
     canvas.addEventListener("mousedown",startPosition);
     canvas.addEventListener("mouseup",finishedPosition);
     canvas.addEventListener("mousemove",draw);
-    
-});
